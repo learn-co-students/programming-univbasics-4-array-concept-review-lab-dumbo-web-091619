a81e86812fc52217faa4bb8e7512afc889455448
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-09-15 18:26:41 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.index(value_to_find) 
 end
 
 def find_max_value(array)
   # Add your solution here
+  array.max_by do |element|
+  element
+  end
 end
 
 def find_min_value(array)
   # Add your solution here
+    array.min_by do |element|
+  element
+  end
 end
